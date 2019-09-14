91f35be3d1baa8e724e3b9c7aff03610beb28f8c
julioleao <julio0023@live.com>
2019-09-14 16:28:12 -0300

    1.0.5
    Add modelo User
    Add interface e método para o repositório de login do usuário

--- MoradaGuia.API/Data/DataContext.cs
+++ MoradaGuia.API/Data/DataContext.cs
@@ -7,5 +7,6 @@ namespace MoradaGuia.API.Data
     {
         public DataContext(DbContextOptions<DataContext> options) : base (options) {}
         public DbSet<Value> Values { get; set; }
+        public DbSet<User> Users { get; set; }
     }
 }
