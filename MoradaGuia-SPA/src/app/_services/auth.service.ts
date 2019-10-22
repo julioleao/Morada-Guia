6c685473357e3d38f6643ac19ab9e9c3644b29fb
MucaBordini <mucabordini@gmail.com>
2019-10-22 13:10:00 -0300

    Revert "client login"
    This reverts commit 7ab800ace3e36abe206d7cd8db24b4aab2f5276b, reversing
    changes made to 10b2822749f2531f4f917a604d4ba96ce3ceeca5.

--- MoradaGuia-SPA/src/app/_services/auth.service.ts
+++ MoradaGuia-SPA/src/app/_services/auth.service.ts
@@ -1,31 +1,17 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-<<<<<<< HEAD
 import {map} from 'rxjs/operators';
-=======
-import { map } from 'rxjs/operators';
-
->>>>>>> afbaa48b5f8ab82db5d4ca6c015042a198bd8868
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
-<<<<<<< HEAD
   baseUrl = 'http://localhost:5000/api/auth/';
-=======
-baseUrl = 'http://localhost:5000/api/auth';
-
->>>>>>> afbaa48b5f8ab82db5d4ca6c015042a198bd8868
 constructor(private http: HttpClient) { }
 
 login(model: any) {
   return this.http.post(this.baseUrl + 'login', model)
   .pipe(
-<<<<<<< HEAD
     map((response: any) => {
-=======
-    map((response: any) =>{
->>>>>>> afbaa48b5f8ab82db5d4ca6c015042a198bd8868
       const user = response;
       if (user) {
         localStorage.setItem('token', user.token);
