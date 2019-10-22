06182de90cdf96979fc3a9f90bfe2790598f62b1
julioleao <julio0023@live.com>
2019-10-22 14:26:07 -0300

    Ajustado model User com os atributos do banco

--- MoradaGuia.API/Models/User.cs
+++ MoradaGuia.API/Models/User.cs
@@ -1,10 +1,22 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace MoradaGuia.API.Models
 {
     public class User
     {
         public int Id { get; set; }
+        [Required][MaxLength(20)]
         public string Username { get; set; }
+        [Key][MaxLength(30)]
+        public string Email { get; set; }
+        [Required][MaxLength(20)]
         public byte[] PasswordHash { get; set; }
+        [Required][MaxLength(20)]
         public byte[] PasswordSalt { get; set; }
+        [Required][MaxLength(40)]
+        public string sobrenome { get; set; }
+        [Required][MaxLength(15)]
+        public int telefone { get; set; }
+        
     }
 }
