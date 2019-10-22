671a2bb4cb5516350753fbb29a9acea46863ae33
MucaBordini <mucabordini@gmail.com>
2019-10-22 18:39:09 -0300

    Adicionado front-end e login

--- MoradaGuia.API/Models/User.cs
+++ MoradaGuia.API/Models/User.cs
@@ -5,17 +5,17 @@ namespace MoradaGuia.API.Models
     public class User
     {
         public int Id { get; set; }
-        [Required][MaxLength(20)]
+        [MaxLength(20)]
         public string Username { get; set; }
-        [Key][MaxLength(30)]
+        [MaxLength(30)]
         public string Email { get; set; }
-        [Required][MaxLength(20)]
+        [MaxLength(20)]
         public byte[] PasswordHash { get; set; }
-        [Required][MaxLength(20)]
+        [MaxLength(20)]
         public byte[] PasswordSalt { get; set; }
-        [Required][MaxLength(40)]
+        [MaxLength(40)]
         public string sobrenome { get; set; }
-        [Required][MaxLength(15)]
+        [MaxLength(15)]
         public int telefone { get; set; }
         
     }
