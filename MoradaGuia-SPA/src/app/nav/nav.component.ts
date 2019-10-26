a37f4e092f6eb9c6d37f334b626b1943a564acec
Alexandre Kawamura Tanaka <alexandre.k.tanaka@gmail.com>
2019-10-25 23:52:35 -0300

    Add angular-jwt para decodificar token

--- MoradaGuia-SPA/src/app/nav/nav.component.ts
+++ MoradaGuia-SPA/src/app/nav/nav.component.ts
@@ -10,13 +10,13 @@ import { AlertifyService } from '../_services/alertify.service';
 export class NavComponent implements OnInit {
   model: any = {};
 
-  constructor(private authService: AuthService, private alertify: AlertifyService) { }
+  constructor(public authService: AuthService, private alertify: AlertifyService) { }
 
   ngOnInit() {
   }
 
 
-  login(){
+  login() {
     this.authService.login(this.model).subscribe(next => {
       this.alertify.success('Logged in successfully');
     }, error => {
@@ -25,11 +25,10 @@ export class NavComponent implements OnInit {
   }
 
   loggedIn() {
-    const token = localStorage.getItem('token');
-    return !!token;
+    return this.authService.loggedIn();
   }
 
-  logout(){
+  logout() {
     localStorage.removeItem('token');
     this.alertify.message('logged out');
   }
