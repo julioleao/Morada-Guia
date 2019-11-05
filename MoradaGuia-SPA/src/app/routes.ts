533975518d48c1ee8e36f01f7e930972dba3f8d4
MucaBordini <mucabordini@gmail.com>
2019-11-05 16:02:31 -0300

    correção conflitos

--- MoradaGuia-SPA/src/app/routes.ts
+++ MoradaGuia-SPA/src/app/routes.ts
@@ -11,11 +11,7 @@ import { ImovelEditComponent } from './imoveis/imovel-edit/imovel-edit.component
 import { ImovelEditResolver } from './_resolvers/imovel-edit.resolver';
 import { ImovelMyListResolver } from './_resolvers/imovel-mylist.resolver';
 import { PreventUnsavedChanges } from './_guards/prevent-unsaved-changes.guard';
-<<<<<<< HEAD
-import { ImovelListToEditComponent } from './imoveis/imovel-listToEdit/imovel-listToEdit.component';
-=======
 import { MyimoveisComponent } from './imoveis/myimoveis/myimoveis.component';
->>>>>>> 2b190af7b8fa6854e36287da9b12874b691ed87e
 
 export const appRoutes: Routes = [
     { path: '', component: HomeComponent},
@@ -27,13 +23,8 @@ export const appRoutes: Routes = [
             { path: 'imoveis', component: ImovelListComponent, resolve: {imoveis: ImovelListResolver}},
             { path: 'myimoveis', component: MyimoveisComponent, resolve: {imoveis: ImovelMyListResolver}},
             { path: 'imoveis/:id', component: ImovelDetailComponent, resolve: {imovel: ImovelDetailResolver}},
-<<<<<<< HEAD
-            { path: 'imovel/edit', component: ImovelListToEditComponent,
-                resolve: {imovel: ImovelListResolver}, canDeactivate: [PreventUnsavedChanges]},
-=======
             { path: 'imovel/edit/:id', component: ImovelEditComponent,
                 resolve: {imovel: ImovelEditResolver}, canDeactivate: [PreventUnsavedChanges]},
->>>>>>> 2b190af7b8fa6854e36287da9b12874b691ed87e
             { path: 'messages', component: MessagesComponent},
             { path: 'lists', component: ListsComponent},
         ]
